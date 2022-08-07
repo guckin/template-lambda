49b6650b4579c01fe1e51465d711c3af06aa9ada
guckin <donald.stephen.mcguckin@gmail.com>
2022-08-06 21:06:08 -0700

    fixes test

--- src/lambda.test.ts
+++ src/lambda.test.ts
@@ -1,7 +1,9 @@
 import {handler} from './lambda';
 
 describe('lambda', () => {
-    it('returns nothing', () => {
-        expect(handler()).toBeFalsy();
+    it('returns nothing', async () => {
+        const result = await handler();
+
+        expect(result).toEqual(undefined);
     });
 });
